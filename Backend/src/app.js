5e75d517a2b4b07ca65afa6a8e587f55338c86ac
Jagruti Patil <patiljagruti658@gmail.com>
2026-04-11 23:50:23 +0530

    solve deployment error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,14 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000));
-app.use(cors());
+app.use(cors({
+     origin: [
+    "http://localhost:5173", // local dev
+    "https://lets-meet-the-video-conferencing-app-eaur.onrender.com" // deployed frontend
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json({limit: "40kb" }));
 app.use(express.urlencoded({limit: "40kb" , extended: true}));
 
